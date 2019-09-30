3d276c28ccde08e274ed96c92540e5be7ffb1a15
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2019-09-30 09:17:10 +0700

    init project

---
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -0,0 +1,32 @@
+package id.ac.polinema.idealbodyweight;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+	// Deklarasikan atribut Fragment di sini
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return  true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		// TODO: Tambahkan penanganan menu di sini
+
+		return super.onOptionsItemSelected(item);
+	}
+}
